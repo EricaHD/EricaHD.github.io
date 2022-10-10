ff6d3cca362a5777b677537ab482b39e25beedf7
EricaHD <erica.h.dominic@gmail.com>
2022-10-09 21:56:50 -0400

    Make separate component for header

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    const body = `
+      <section id="banner">
+        <div class="inner">
+          <div id="title_container"></div>
+          <p>Software Engineer<br/>New York, NY</p>
+          <ul class="icons">
+            <li>
+              <a href="https://www.linkedin.com/in/ericahd" class="icon fa-linkedin">
+                <span class="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:erica.h.dominic@gmail.com" class="icon fa-envelope-o">
+                <span class="label">Email</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/EricaHD" class="icon fa-github">
+                <span class="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <a href="#one" class="more scrolly"></a>
+      </section>
+    `;
+    return (<div dangerouslySetInnerHTML={{__html: body}} />);
+  }
+}
+
+export default Header;
