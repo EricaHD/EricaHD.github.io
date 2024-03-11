01775ef112e12ba6b23e0c40bc38a8acc0aff8db
EricaHD <erica.h.dominic@gmail.com>
2024-03-11 01:35:16 -0400

    Add MUI bar chart

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BarChart } from '@mui/x-charts/BarChart';
 import Button from '@mui/material/Button';
 
 class Header extends Component {
@@ -8,6 +9,12 @@ class Header extends Component {
         <Button variant="contained">Hello world</Button>
         <p>Sorry you cannot view my personal website at this time!</p>
         <p>I am borrowing this page temporarily for a hackathon project.</p>
+        <BarChart
+          xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+          series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+          width={500}
+          height={300}
+        />
       </div>
     );
   }
