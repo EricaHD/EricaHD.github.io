75dc0589f3affe5299757efe52c68e9bc900da1d
EricaHD <erica.h.dominic@gmail.com>
2024-03-14 12:29:21 -0400

    Add header and subheaer to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Typography from '@mui/material/Typography';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Typography variant="h3" component="div" sx={{ textAlign: 'center' }}>
+          Retirement Contribution Calculator
+        </Typography>
+        <Typography variant="h6" component="div" sx={{ textAlign: 'center' }}>
+          with a 2% per-paycheck match from your company
+        </Typography>
         <Header />
       </div>
     );
